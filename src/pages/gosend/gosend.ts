016f485c3b6a493c60264e953bf458ebe0ab417c
Kehinde Kolade <kolexinfos@gmail.com>
2016-12-11 09:39:04 +0100

    Added Distance API implementation

--- src/pages/gosend/gosend.ts
+++ src/pages/gosend/gosend.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
+import { Toast } from 'ionic-native';
 
 import { ModalAutocompleteItems } from '../modal-autocomplete-items/modal-autocomplete-items';
 declare var google:any;
@@ -31,7 +32,45 @@ export class GosendPage {
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
 
  goSendRequest(form){
+     console.log(form);
 
+
+     var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+        {
+            origins: [this.gosend.location],
+            destinations: [this.gosend.destination ],
+            travelMode: 'DRIVING',
+            avoidHighways: false,
+            avoidTolls: false,
+        }, callback);
+
+        function callback(response, status) {
+            console.log(status);
+            console.log(response);
+                    if (status == 'OK') {
+                var origins = response.originAddresses;
+                var destinations = response.destinationAddresses;
+
+                for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+                for (var j = 0; j < results.length; j++) {
+                    var element = results[j];
+                    var distance = element.distance.text;
+                    var duration = element.duration.text;
+                    var from = origins[i];
+                    var to = destinations[j];
+                    var info = "The distance from " + from + " to " + to + " is " + distance + " - " + "and would take approximately " + duration;
+                    console.log(info);
+
+                    Toast.show(info, '10000', 'center').subscribe(
+                        toast => {
+                            console.log(toast);
+                        });
+                    }
+                }
+            }
+        }
  }
 
   showMap(){
